212fe22348a74c08cc2ca41fb9a3a46f4bc7df02
Prasad Telawane <prasad@kores-india.com>
2020-03-27 17:20:44 +0530

    First commit

---
+++ GetDetails/Controllers/GetDetailsController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace GetDetails_in_ASP.NET_MVC.Controllers
+{
+    public class GetDetailsController : Controller
+    {
+        // GET: GetDetails
+        public ActionResult Index()
+        {
+
+
+            String constring = "Data Source=10.168.102.11,2433; Initial Catalog=iKloudProSMB; Integrated Security=true";
+            SqlConnection sqlcon = new SqlConnection(constring);
+            String pname = "GetDetails";
+            sqlcon.Open();
+            SqlCommand com = new SqlCommand(pname, sqlcon);
+            com.CommandType = CommandType.StoredProcedure;
+            SqlDataReader dr = com.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            return View(dt);
+
+
+        }
+
+        [HttpPost]
+        public ActionResult Index(string cbutton)
+        {
+
+            if (cbutton == "send")
+            {
+                String constring = "Data Source=10.168.102.11,2433; Initial Catalog=iKloudProSMB; Integrated Security=true";
+                SqlConnection sqlcon = new SqlConnection(constring);
+                String pname = "UpdateCHIStatus"; ;
+                sqlcon.Open();
+                SqlCommand com = new SqlCommand(pname, sqlcon);
+                com.CommandType = CommandType.StoredProcedure;
+                com.ExecuteNonQuery();
+            }
+
+            return null;
+        }
+
+        [HttpPost]
+        public ActionResult Unmark(int ID)
+        {
+
+
+            String constring = "Data Source=10.168.102.11,2433; Initial Catalog=iKloudProSMB; Integrated Security=true";
+            SqlConnection con = new SqlConnection(constring);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "UpdateCHIStatusandReturnCode";
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlParameter param = new SqlParameter("ID", ID);
+            cmd.Parameters.Add(param);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            return null;
+        }
+
+    }
+}
